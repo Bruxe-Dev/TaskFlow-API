1dec73c7eece7b4449f64a75b3d7a4ef23a5b27d
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-13 20:01:32 +0200

    Just added a Projects Mongoose Schema that structures how the data will be stored and handled

---
+++ models/Project.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        title: String,
+        required: [true, 'Project is required'],
+        trim: true,
+        maxLength: [100, 'Projet name can not exceed 100 Characters'],
+    },
+    description: {
+        type: String,
+        trim: true,
+        maxLength: [500, 'The projet description cannot exceed 500 characters']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Project', projectSchema);
--- server.js
+++ server.js
@@ -2,14 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.middleware(express.json());
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
 
     .then(() => {
         console.log("MongoDB connected Succesfully")
     })
-    .catch(() +> {
-
+    .catch((err) => {
+        console.error("MongoDB failed to connect successfully", err)
     })
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to the TaskFlow API" })
+})
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
