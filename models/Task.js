406e1ef73eab8ec9345b6ac7361d5af3df54817e
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-14 08:55:51 +0200

    added a prehook  in the task module in order to keep track of the task id

--- models/Task.js
+++ models/Task.js
@@ -1,6 +1,11 @@
 const { default: mongoose } = require('mongoose')
+const Counter = require('./Counter')
 
 const taskSchema = new mongoose.Schema({
+    taskId: {
+        type: Number,
+        unique: true
+    },
     title: {
         type: String,
         required: [true, 'Task title is required'],
@@ -29,6 +34,21 @@ const taskSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-})
+});
 
-module.exports = mongoose.model('Task', taskSchema);
+//Auto incrementaion of ID
+taskSchema.pre('save', async () => {
+    if (!this.isNew) return next();
+
+    const counter = await Counter.findByIdAndUpdate(
+        { _id: 'taskId' },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+    );
+
+    this.taskId = counter.seq;
+    next()
+})
+const counter =
+
+    module.exports = mongoose.model('Task', taskSchema);
