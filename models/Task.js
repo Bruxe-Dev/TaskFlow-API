7ea7c682759757e08eba4a97e64112c0f0391f82
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-14 15:20:56 +0200

    added the get and post routes

--- models/Task.js
+++ models/Task.js
@@ -2,10 +2,6 @@ const { default: mongoose } = require('mongoose')
 const Counter = require('./Counter')
 
 const taskSchema = new mongoose.Schema({
-    taskId: {
-        type: Number,
-        unique: true
-    },
     title: {
         type: String,
         required: [true, 'Task title is required'],
@@ -36,19 +32,4 @@ const taskSchema = new mongoose.Schema({
     }
 });
 
-//Auto incrementaion of ID
-taskSchema.pre('save', async () => {
-    if (!this.isNew) return next();
-
-    const counter = await Counter.findByIdAndUpdate(
-        { _id: 'taskId' },
-        { $inc: { seq: 1 } },
-        { new: true, upsert: true }
-    );
-
-    this.taskId = counter.seq;
-    next()
-})
-const counter =
-
-    module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model('Task', taskSchema);
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,10 +1,71 @@
 const express = require('express')
 const router = express.Router();
-const Task = require('../models/Task')
+const Task = require('../models/Task');
+const Project = require('../models/Project')
 
 router.post('/', async (req, res) => {
     try {
-        const { title, description, dueDate, project } = req.body
+        const { title, description, dueDate, project } = req.body;
+
+        const projectExist = await Project.findById(project);
+        if (!projectExist) {
+            return res.status(404).json({
+                success: false,
+                error: "Project Not found"
+            })
+        }
+        const task = new Task({
+            title,
+            description,
+            dueDate,
+            project,
+        });
+
+        await task.save();
+
+        res.status(200).json({
+            success: true,
+            data: task
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Project.find().populate('project', 'name description');
+
+        if (!tasks) {
+            res.status(404).json({
+                success: false,
+                error: "No Tasks Available!"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            count: tasks.lenght,
+            data: tasks
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
+});
+
+router.get('/project/:projectId', async (req, res) => {
+    try {
+        const tasks = await Task.find({
+            project: req.params.projectId
+        }).populate('project', 'name description')
+
+        res.status(200).json({})
     } catch (error) {
 
     }
