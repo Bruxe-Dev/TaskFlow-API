8c202a6a416abdad0ffd0a96a47990d4c94c0e1d
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-15 08:53:43 +0200

    added the loggind middleware in the mainserver

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db')
 
+const logger = require('./middleware/reqLogger');
+const errorHandler = require('./middleware/errorHandler');
+
 connectDB(); //Connecting to DB
 
 app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(logger)
+}
 
 const projectRoutes = require('./routes/projectRoute'); //Import Project routes
 app.use('/api/projects', projectRoutes) //Use the routes
@@ -16,6 +23,6 @@ app.use('/api/tasks', taskRoutes)
 
 
 app.get('/', (req, res) => {
-    res.json({ message: "Welcome to the TaskFlow API" })
+    res.json({ message: "Welcome to TaskFlow " })
 })
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
