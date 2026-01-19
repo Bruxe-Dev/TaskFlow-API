ec43aa4c4c180caadcf1474ae5669099310c90b2
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-19 19:44:00 +0200

    created the users schema

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    c
+})
