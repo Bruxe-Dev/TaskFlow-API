6f16945cadda56052d8cf61cf704b10a0b2c9baf
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-25 20:58:30 +0200

    renamed the file name and added a way to  send email and created a transporter

---
+++ utils/sendEmail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+    let transporter;
+
+    if (process.env.NODE_ENV === 'production') {
+        transporter = nodemailer.createTransport({
+            service: process.env.EMAIL_SERVICE,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+    } else {
+        // Development: Log to console instead
+        transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'ethereal.user@ethereal.email',
+                pass: 'ethereal.password'
+            }
+        });
+
+        // For development, we'll just log the email
+        console.log('\n EMAIL WOULD BE SENT:');
+        console.log('To:', options.email);
+        console.log('Subject:', options.subject);
+        console.log('Message:', options.message);
+        console.log('\n');
+
+        return; // Don't send in development
+    }
+}
