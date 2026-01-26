2c09431e7db1aa790c42eb73117aaa875ea08225
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-26 19:59:09 +0200

    debugge some error and added a missing module now reamining to test the api out

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const asyncHandler = require('../middleware/asyncHandler');
+const asyncHandler = require('../middleware/asyncHandlewrapp');
 const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 
