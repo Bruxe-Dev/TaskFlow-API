f94647b8cac181b2bdf01fcc00b623def9cf5534
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-28 15:11:58 +0200

    added some of the required field

--- models/pendingUser.js
+++ models/pendingUser.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const { type } = require('os');
 require('dotenv').config();
 
 const pendingUserSchema = new mongoose.Schema({
@@ -16,5 +17,13 @@ const pendingUserSchema = new mongoose.Schema({
         lowercase: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
             'Please provide a valid email']
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    emailVerificationToken: {
+        type: String,
+        required: true
     }
 })
