4e99e6c097499fa736485c26e6a8e87c1c3fa726
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-28 15:09:57 +0200

    making the pending users schema  ready to finish it up

--- models/pendingUser.js
+++ models/pendingUser.js
@@ -1,6 +1,20 @@
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const { type } = require('os');
 require('dotenv').config();
+
+const pendingUserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please provide a valid email']
+    }
+})
