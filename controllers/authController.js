6290ee92197e9ed9a5206bba7a076f647f1124ae
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-07 16:01:55 +0200

    added dotenv to the authController file because it was resulting into the failure to fetch the actural CLIENT_URL  from the environment variables file

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const asyncHandler = require('../middleware/asyncHandlewrapp');
 const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
+require('dotenv').config();
 
 exports.register = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
