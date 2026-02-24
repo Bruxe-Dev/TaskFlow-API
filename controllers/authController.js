825354c9cc3e2df3f27c92bd69e96b510c0ecdcc
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-24 21:23:09 +0200

    removed multiple .env config calls

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const User = require('../models/User');
 const PendingUser = require('../models/pendingUser');
 const asyncHandler = require('../middleware/asyncHandlewrapp');
