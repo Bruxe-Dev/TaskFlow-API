e728387cded04bbc40148bce1881a2313d47797e
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-02 19:49:06 +0200

    moved all the possible files into the src folder

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const PendingUser = require('../models/pendingUser');
 const asyncHandler = require('../middleware/asyncHandlewrapp');
-const sendEmail = require('../../utils/sendEmail');
+const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
