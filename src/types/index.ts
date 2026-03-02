c963bb97477ad3a9edc3ceb0fe74b96e557bfb1d
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-02 19:58:41 +0200

    declared the project inum structures

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,15 @@
 import { Document, Types } from 'mongoose';
 
 export enum userRole {
+    ORG_LEADER = 'org_leader',
+    FIELD_ADMIN = 'field_admin',
+    MEMBER = 'member'
+}
 
+export enum projectStatus {
+    PENDING = 'pending',
+    IN_PROGRESS = 'in_progress',
+    REVIEW = 'review',
+    COMPLETED = 'completed',
+    OVERDUE = 'overdue'
 }
