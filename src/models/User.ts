58cc1b065e180eceb7fdcfb7df12be12f9fafaae
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-04 15:21:03 +0200

    added a username filed in the  User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,3 +6,13 @@ import jwt from 'jsonwebtoken'
 import crypto from 'crypto'
 import { IUser, UserRole } from '../types/index'
 
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: [true, 'Please enter a username'],
+        trim: true,
+        unique: true,
+        minLength: [4, 'Username must ne 6 Chars min'],
+        maxLength: [8, 'Username must not overceed 8Chars']
+    }
+})
