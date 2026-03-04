74cc293219985a63c51751a199c6486d7c717c09
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-04 19:43:35 +0200

    added some fields to the field model

--- src/models/Field.ts
+++ src/models/Field.ts
@@ -1,2 +1,28 @@
-import { Model, Schema } from 'mongoose'
+import { Model, Schema, Types } from 'mongoose'
 import { IField } from '../types'
+import { settings } from 'node:cluster'
+
+const fieldSchema = new Schema<IField>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: [30, 'Field Length can not be greater than 30 Chars']
+    },
+    description: {
+        type: String,
+        maxLength: [500, 'Description must not be more than 500 characters'],
+        trim: true
+    },
+    organization: {
+        type: Schema.Types.ObjectId,
+        ref: 'Organisation',
+        required: true
+    },
+    admin: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        trim: true
+    }
+})
