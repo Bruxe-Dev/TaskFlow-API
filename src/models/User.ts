df61d6c1dbb7a03a549d070a0b581e977c2d74c6
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-04 15:26:30 +0200

    added the email na password fields

--- src/models/User.ts
+++ src/models/User.ts
@@ -14,5 +14,21 @@ const userSchema = new Schema<IUser>({
         unique: true,
         minLength: [4, 'Username must ne 6 Chars min'],
         maxLength: [8, 'Username must not overceed 8Chars']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter you email'],
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please provide a valid email'],
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please a password'],
+        unique: true,
+        select: false,
+        minLength: [6, 'Password must be 6 chars minimum']
     }
 })
