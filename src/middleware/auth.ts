c654b80f5e94490c93e5abf7365f39654f6f40b2
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-21 21:26:24 +0200

    finished the token extraction method

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,3 +2,36 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import { IUser, UserRole } from '../types';
+import { totalmem } from 'node:os';
+
+export interface AuthRequest extends Request {
+    user?: IUser
+}
+
+export const protect = async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    let token: string | undefined;
+
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (authHeader && typeof authHeader === 'string' && authHeader.startsWith('Bearer')) {
+        token = authHeader.split(' ')[1]
+    }
+
+    if (!token) {
+        res.status(401).json({
+            success: false,
+            message: "Not Authorised to acces this route"
+        })
+        return;
+    }
+
+    try {
+
+    } catch (error) {
+
+    }
+}
