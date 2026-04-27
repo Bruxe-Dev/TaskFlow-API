8ed2259d718b51a871e04ed3a125a0598deb6683
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-27 11:45:48 +0200

    added a function to assign the user an organization since he created it

--- src/controllers/organisationController.ts
+++ src/controllers/organisationController.ts
@@ -19,5 +19,20 @@ export const getOrganisation = asyncHandleWrapper(async (req: AuthRequest, res: 
             success: false,
             message: "You are already enrolled in this Organization! Leave you current Organization first"
         })
+        return;
     }
+
+    const organization = await Organization.create({
+        name,
+        description,
+        industry,
+        leader: req.user?._id,
+        maxAdmins: 6,
+        activeAdmins: 0
+    })
+
+    await User.findByIdAndUpdate(req.user?._id, {
+        role: UserRole.ORG_LEADER,
+        organization: organization._id
+    })
 })
