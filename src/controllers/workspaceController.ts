49cafc85968fb1f089a2beedd171d15f52756686
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-29 14:41:25 +0200

    debugged the imports

--- src/controllers/workspaceController.ts
+++ src/controllers/workspaceController.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
 import { AuthRequest } from '../middleware/auth';
-import asyncHandler from '../middleware/asyncHandler';
+import asyncHandlewrapper from '../middleware/asyncHandlewrapp';
 import { Workspace, Team, Project, Update, User } from '../models';
 import { UserRole } from '../types';
