b0b63da68c11277d68add883a0429c833a25822b
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-29 20:03:26 +0200

    added a function for Viewing workspaces details

--- src/controllers/workspaceController.ts
+++ src/controllers/workspaceController.ts
@@ -9,3 +9,51 @@ import { UserRole } from '../types';
  * @route GET /api/workspaces/:id
  * @access Private (Only team Members can access this)
  */
+
+export const getWorkspace = asyncHandlewrapper(async (req: AuthRequest, res: Response) => {
+    const workspace = await Workspace.findById(req.params.id)
+        .populate('team')
+        .populate({
+            path: 'projects',
+            populate: {
+                path: 'assignedBy tasks',
+                select: 'username title status'
+            }
+        });
+
+    if (!workspace) {
+        res.status(404).json({
+            success: false,
+            message: "No Worospace found"
+        })
+    }
+
+    //Check if he is the member of the team
+
+    const team = await Team.findById(workspace?.team)
+
+    if (!team) {
+        res.status(404).json({
+            succcess: false,
+            message: "Field Not Found"
+        })
+        return;
+    }
+    const isMember =
+        team.members.some(
+            (member: any) => member.user.toString() === req.user?.isDirectModified.toString()
+        )
+
+    if (!isMember && req.user?.role !== UserRole.ORG_LEADER) {
+        res.status(403).json({
+            success: false,
+            message: "UNAUTHORIZED"
+        })
+        return;
+    }
+
+    res.status(200).json({
+        success: true,
+        data: workspace
+    })
+})
