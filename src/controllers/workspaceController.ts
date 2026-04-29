9060e7c5452147e5327d867b5c164d438dbe7e1b
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-29 14:43:57 +0200

    added some Documentatrion Comments Templates

--- src/controllers/workspaceController.ts
+++ src/controllers/workspaceController.ts
@@ -3,3 +3,9 @@ import { AuthRequest } from '../middleware/auth';
 import asyncHandlewrapper from '../middleware/asyncHandlewrapp';
 import { Workspace, Team, Project, Update, User } from '../models';
 import { UserRole } from '../types';
+
+/**
+ * @desc Get Workspace details
+ * @route GET /api/workspaces/:id
+ * @access Private (Only team Members can access this)
+ */
