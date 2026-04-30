8a8c74b19005800c9ca99cde679454276aafb67a
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-30 20:22:34 +0200

    Finished up the submission routes file

--- src/routes/submissionRoutes.ts
+++ src/routes/submissionRoutes.ts
@@ -1,2 +1,18 @@
 import express from "express";
-import { } from '../controllers/submissionController'
+import {
+    createSubmission,
+    getSubmission,
+    getPendingSubmissions,
+    reviewSubmission
+} from '../controllers/submissionController';
+
+import { protect, isFieldAdmin } from "../middleware/auth";
+
+const router = express.Router();
+
+router.post('/', protect, createSubmission);
+router.get('/pending', protect, isFieldAdmin, getPendingSubmissions);
+router.get('/:id', protect, getSubmission);
+router.post('/:id/review', protect, isFieldAdmin, reviewSubmission);
+
+export default router;
